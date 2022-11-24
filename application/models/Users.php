8a3478f1c55f36a3bcabb6a7325719f58f949674
msantosEmtech <mario.santos@emtech.digital>
2022-11-24 11:34:28 -0600

    Modelo users y base de datos

---
+++ application/models/Users.php
@@ -0,0 +1,153 @@
+<?php
+class Users extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function Get(){
+        $this->db->select("
+        users.Id            AS user_id, 
+        users.Name          AS user_name,
+        users.LastName      AS user_last_name,
+        users.Email         AS user_email,
+        users.IdGender      AS user_id_gender,
+        users.CountryCode   AS user_country_code,
+        users.Phone         AS user_phone,
+        users.RegisterDate  AS user_register_date,
+        users.Birthday      AS user_birthday,
+        users.Password      AS user_password,
+        users.Status        AS user_status,
+        genders.Description AS user_gender_description");
+		$this->db->from("users");
+        $this->db->join('genders', 'genders.Id = users.IdGender', 'inner');
+
+		return $this->db->get()->result_array();
+    }
+
+    public function GetById($id){
+        $condicional = array('users.Id' => $id);
+
+        $this->db->select("
+        users.Id            AS user_id, 
+        users.Name          AS user_name,
+        users.LastName      AS user_last_name,
+        users.Email         AS user_email,
+        users.IdGender      AS user_id_gender,
+        users.CountryCode   AS user_country_code,
+        users.Phone         AS user_phone,
+        users.RegisterDate  AS user_register_date,
+        users.Birthday      AS user_birthday,
+        users.Password      AS user_password,
+        users.Status        AS user_status,
+        genders.Description AS user_gender_description");
+		$this->db->from("users");
+        $this->db->join('genders', 'genders.Id = users.IdGender', 'inner');
+        $this->db->where($condicional);
+
+		return $this->db->get()->row_array();
+    }
+
+    public function GetByEmail($email){
+        $condicional = array('users.Email' => $email);
+
+        $this->db->select("
+        users.Id            AS user_id, 
+        users.Name          AS user_name,
+        users.LastName      AS user_last_name,
+        users.Email         AS user_email,
+        users.IdGender      AS user_id_gender,
+        users.CountryCode   AS user_country_code,
+        users.Phone         AS user_phone,
+        users.RegisterDate  AS user_register_date,
+        users.Birthday      AS user_birthday,
+        users.Password      AS user_password,
+        users.Status        AS user_status,
+        genders.Description AS user_gender_description");
+		$this->db->from("users");
+        $this->db->join('genders', 'genders.Id = users.IdGender', 'inner');
+        $this->db->where($condicional);
+
+		return $this->db->get()->row_array();
+    }
+
+    public function GetByCredentials($email, $pasword){
+        $condicional = array('users.Email' => $email, 'users.Password' => $pasword);
+
+        $this->db->select("
+        users.Id            AS user_id, 
+        users.Name          AS user_name,
+        users.LastName      AS user_last_name,
+        users.Email         AS user_email,
+        users.IdGender      AS user_id_gender,
+        users.CountryCode   AS user_country_code,
+        users.Phone         AS user_phone,
+        users.RegisterDate  AS user_register_date,
+        users.Birthday      AS user_birthday,
+        users.Password      AS user_password,
+        users.Status        AS user_status,
+        genders.Description AS user_gender_description");
+		$this->db->from("users");
+        $this->db->join('genders', 'genders.Id = users.IdGender', 'inner');
+        $this->db->where($condicional);
+
+		return $this->db->get()->row_array();
+    }
+
+    public function Add($datos){
+        $exist = $this->db->query("SELECT Id FROM users WHERE Email ='".$datos['Email']."'");
+
+		$result = $exist->num_rows();
+
+		if($result > 0){
+			return false;
+		}else{
+			$data = [
+				'Name'          => $datos['Name'],
+				'LastName'      => $datos['LastName'],
+				'Email'         => $datos['Email'],
+				'IdGender'      => $datos['IdGender'],
+				'CountryCode'   => $datos['CountryCode'],
+				'RegisterDate'  => $datos['RegisterDate'],
+				'Birthday'      => $datos['Birthday'],
+                'Password'      => $datos['Password'],
+                'Status'        => $datos['Status']
+			];
+
+			return $this->db->insert("users", $data);
+		}
+    }
+
+    public function Update($datos, $id){
+
+        $exist = $this->db->query("SELECT Id FROM users WHERE Id <> '.$id.' AND Email ='".$datos['Email']."'");
+
+        $result = $exist->num_rows();
+
+		if($result > 0){
+			return false;
+		}else{
+        $where = array('Id' => $id);
+
+        $data = [
+            'Name'          => $datos['Name'],
+            'LastName'      => $datos['LastName'],
+            'Email'         => $datos['Email'],
+            'IdGender'      => $datos['IdGender'],
+            'CountryCode'   => $datos['CountryCode'],
+            'RegisterDate'  => $datos['RegisterDate'],
+            'Birthday'      => $datos['Birthday'],
+            'Password'      => $datos['Password'],
+            'Status'        => $datos['Status']
+        ];
+
+        return $this->db->update("users", $data, $where);
+        }
+    }
+
+    public function Delete($id){
+		$where = array('Id' => $id);
+		return $this->db->delete('users', $where);
+	}
+}
