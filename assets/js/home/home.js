15e38646c37686a0ddbb2992779bb1b13bd6ea9c
Impulsa Dev <106106621+impulsadev@users.noreply.github.com>
2022-11-29 16:35:49 -0600

    Tiktok

--- assets/js/home/home.js
+++ assets/js/home/home.js
@@ -116,3 +116,38 @@
             // instead of a settings object
         ]
     });
+
+    var feed = new Instafeed({
+        accessToken: 'IGQVJYOUpUXzNvZAU1BdVgwUWtsVzJFeVY3ZAHFEZAEpUdEJyNzVyQ2ZAseDdHanpuMHlGSURZAME5ISWdESXR0dl95bFJiX0M0Vk9kNWY5WEhCWmI0aTFiMEtHOHdqNmhScFFwYzJuaVZAfc2FVa1JsSmpMeAZDZD',
+        limit:3,
+        template:'<div class="item"><a href="{{link}}"><img title="{{caption}}" src="{{image}}" class="imgSliderIns shadow-1-strong rounded mt-5 mb-4" /></a></div>',
+        after: function(){
+            $('.owl-carousel').owlCarousel({
+                loop:true,
+                margin:10,
+                autoplay:true,
+                autoplayTimeout:3000,
+                autoplayHoverPause:true,
+                responsiveClass:true,
+                responsive:{
+                    0:{
+                        items:2,
+                        nav:false,
+                        loop:true
+                    },
+                    600:{
+                        items:2,
+                        nav:false,
+                        loop:true
+                    },
+                    1000:{
+                        items:3,
+                        nav:false,
+                        loop:true
+                    }
+                }
+            });
+        }
+    });
+
+    feed.run();
---
+++ assets/js/login/login.js
@@ -0,0 +1,114 @@
+(function(){
+    "use strict";
+
+    trepcamp = trepcamp ? trepcamp : {};
+
+    trepcamp.login = trepcamp.login ? trepcamp.login : {};
+
+    trepcamp.login.elements = {
+        $txtEmail: $('#txtEmail'),
+        $txtPassword: $('#txtPassword'),
+        $txtPassword2: $('#txtPassword2'),
+        $btnSignUp: $('#btnSignUp'),
+        $formSignUp: $('#formSignUp'),
+
+        $txtEmailSignIn: $('#txtEmailSignIn'),
+        $txtPasswordSignIn: $('#txtPasswordSignIn'),
+        $btnSignIn: $('#btnSignIn'),
+        $formSignIn: $('#formSignIn')
+    };
+
+    trepcamp.login.uris = {
+        add: `${trepcamp.base.url}Login/Insert`,
+        startSession: `${trepcamp.base.url}Login/InitSession`
+    };
+
+    const elements = trepcamp.login.elements;
+    const uris = trepcamp.login.uris;
+
+    function validPasswordEquals(pass1, pass2){
+        if(pass1 != pass2){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    elements.$formSignUp.on("submit", function(e){
+        e.preventDefault();
+
+        let valid = validPasswordEquals(elements.$txtPassword.val().trim(), elements.$txtPassword2.val().trim());
+
+        if(valid){
+            let model = {
+                'email': elements.$txtEmail.val().trim(),
+                'password': elements.$txtPassword.val().trim()
+            }
+
+            
+            $.post(uris.add, model, "JSON")
+            .done(function(result){
+                
+                let res = JSON.parse(result);
+                if(res){
+                    Swal.fire('a verification email has been sent');
+                        elements.$txtEmail.val("");
+                        elements.$txtPassword.val("");
+                        elements.$txtPassword2.val("");
+                }else{
+                    Swal.fire('error');
+                    elements.$txtEmail.val("");
+                        elements.$txtPassword.val("");
+                        elements.$txtPassword2.val("");
+                }
+            });
+
+        }else{
+            Swal.fire('no is same password');
+        }
+    });
+
+    elements.$formSignIn.on("submit", function(e){
+        e.preventDefault();
+        
+        var model = {
+            'email': $("#txtEmailSignIn").val(),
+            'password': $("#txtPasswordSignIn").val()
+        }
+
+        $.post(uris.startSession,model)
+        .done(function(result){
+            var data = JSON.parse(result);
+
+            if(data != false){
+                var titulo = "Access success";
+                Swal.fire({
+                    position: 'center',
+                    icon: 'success',
+                    title: titulo,
+                    showConfirmButton: false,
+                    timer: 1500
+                }).then(result => {
+                    if(result){
+                        window.location.replace(`${trepcamp.base.url}Account`);
+                    }
+                });
+                
+            }else{
+                var titulo = "Email or password Incorrect";
+                alertaMensajeDenied(titulo);
+                
+            }
+        });
+    });
+
+    function alertaMensajeDenied(titulo){
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: titulo,
+            showConfirmButton: false,
+            timer: 1500
+        });
+    };
+}());
