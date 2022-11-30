4a3264578f7793b3515650c3c807f293763c94d5
Oscar Villicana <oscar.valencia@emtech.digital>
2022-11-30 10:23:47 -0600

    update de cambios actuales

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Login extends CI_Controller {
 
     public function __construct(){
+        
 		parent::__construct();
 
         header('Access-Control-Allow-Origin: *');
